c0ca5512f1601d7a730a29f1d8b7f552cfd8a966
CedricPradels <cedric.pradels@gmail.com>
2020-02-19 14:46:00 +0100

    Minimal search with back and marvel api

--- src/containers/Characters.js
+++ src/containers/Characters.js
@@ -1,17 +1,33 @@
 // REACT
-import React from "react";
+import React, { useState } from "react";
+
+// AXIOS
+import axios from "axios";
 
 const Characters = () => {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
   return (
     <main>
       <form
-        onSubmit={event => {
+        onSubmit={async event => {
+          console.log("ahah");
           event.preventDefault();
+          const response = await axios.get(
+            `http://localhost:4000/test?name=${search}`
+          );
+          setResults(response.data);
         }}
       >
         <input
           type="text"
           placeholder="Entrer le nom d'un personnage (ex. : Hulk)"
+          value={search}
+          onChange={event => {
+            event.preventDefault();
+            setSearch(event.target.value);
+          }}
         ></input>
         <button type="submit">Rechercher</button>
       </form>
